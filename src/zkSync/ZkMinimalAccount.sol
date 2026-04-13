8504643db97adfca81cf8440a8d76e675c3d5842
barnabas <barnabasmunuhe@gmail.com>
2026-04-13 20:55:08 +0300

    feat: Implementing fee payment in zksync native account abstraction feat:Implementing external Transaction Execution to enable third party to relay and Pay for the transaction

--- src/zkSync/ZkMinimalAccount.sol
+++ src/zkSync/ZkMinimalAccount.sol
@@ -6,7 +6,7 @@ pragma solidity 0.8.24;
 import {IAccount, ACCOUNT_VALIDATION_SUCCESS_MAGIC} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
 import {Transaction, MemoryTransactionHelper} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
 import {SystemContractsCaller} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/SystemContractsCaller.sol";
-import {NONCE_HOLDER_SYSTEM_CONTRACT, BOOTLOADER_FORMAL_ADDRESS} from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
+import {NONCE_HOLDER_SYSTEM_CONTRACT, BOOTLOADER_FORMAL_ADDRESS, DEPLOYER_SYSTEM_CONTRACT} from "lib/foundry-era-contracts/src/system-contracts/contracts/Constants.sol";
 import {INonceHolder} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/INonceHolder.sol";
 import {Utils} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/Utils.sol";
 // Oz imports
@@ -21,6 +21,8 @@ contract ZkMinimalAccount is IAccount, Ownable{
     error ZkMininimalAccount__NotFromBootLoader();
     error ZkMinimalAccount__ExecutionFailed();
     error ZkMininimalAccount__NotFromBootLoaderOrOwner();
+    error ZkMinimalAccount__FailedToPay();
+    error ZkMinimalAccount__InvalidSignature();
 
     modifier requireFromBootLoader(){
         if(msg.sender != BOOTLOADER_FORMAL_ADDRESS){
@@ -36,8 +38,9 @@ contract ZkMinimalAccount is IAccount, Ownable{
         _;
     }
 
-    constructor()Ownable(msg.sender) {
-    }
+    constructor()Ownable(msg.sender) {}
+
+    receive() external payable {} //contract  can now receive funds
 // phase 1 Validation
 // -users send the transacton to the "zkSymc API client" sort of "light node"
 // - zkSync API client checks to see the nonce is unique by quering the NounceHolder system contract(off-chain)
@@ -68,7 +71,53 @@ contract ZkMinimalAccount is IAccount, Ownable{
         requireFromBootLoader
         returns (bytes4 magic)
         {
-            // call nonce holder
+            return _validateTransaction(_transaction);
+        }
+
+    /**
+     * @notice  .
+     * @dev     .
+     * @param   _transaction  .
+     */
+    function executeTransaction(bytes32 /*_txHash*/, bytes32 /*_suggestedSignedHash*/, Transaction calldata _transaction)
+        external
+        payable
+        requireFromBootLoaderOrOwner
+        {
+            _executeTransaction(_transaction);
+        }
+
+    function executeTransactionFromOutside(Transaction memory _transaction) external payable
+    {
+        bytes4 magic = _validateTransaction(_transaction);
+        if (magic != ACCOUNT_VALIDATION_SUCCESS_MAGIC){
+            revert ZkMinimalAccount__InvalidSignature();
+        }
+        _executeTransaction(_transaction);
+    }
+
+    function payForTransaction(bytes32 /*_txHash*/, bytes32 /*_suggestedSignedHash*/, Transaction memory _transaction)
+        external
+        payable
+        {
+            bool success = _transaction.payToTheBootloader();
+            if(!success){
+                revert ZkMinimalAccount__FailedToPay();
+            }
+        }
+
+    function prepareForPaymaster(bytes32 _txHash, bytes32 _possibleSignedHash, Transaction memory _transaction)
+        external
+        payable
+        {}
+
+
+    /*//////////////////////////////////////////////////////////////
+                           INTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function _validateTransaction(Transaction memory _transaction) internal returns (bytes4 magic){
+        // call nonce holder
             // incement nonce
             SystemContractsCaller.systemCallWithPropagatedRevert(
                 uint32(gasleft()),
@@ -93,43 +142,25 @@ contract ZkMinimalAccount is IAccount, Ownable{
                 magic = bytes4(0);
             }
             return magic;
-        }
+    }
 
-    function executeTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction calldata _transaction)
-        external
-        payable
-        requireFromBootLoaderOrOwner
-        {
-            address to = address(uint160(_transaction.to)); // to is the callee, we need to convert it back to an address from uint256
-            uint128 value = Utils.safeCastToU128(_transaction.value); 
-            bytes32 memory data = _transaction.data; 
+    function _executeTransaction(Transaction memory _transaction) internal {
+        address to = address(uint160(_transaction.to)); // to is the callee(the contract being called), we need to convert it back to an address(20 bytes) from uint256(32 bytes) that's why we need to convert it first to uint160 and then to address
+            uint128 value = Utils.safeCastToU128(_transaction.value); //we might use value as a system contract call and it takes uint128 as input(we use Utils to safeCast this operation), so we need to make sure it fits in uint128 to avoid overflow, also we can save some gas by using uint128 instead of uint256 for value since most transactions won't need more than 2^128 wei which is a very large amount of ether (over 340 billion ether)
+            bytes memory data = _transaction.data; 
 
-            if( to == address(DEPLOYER_SYSTEM_CONTRACT)){
+            if( to == address(DEPLOYER_SYSTEM_CONTRACT)){ // can be customized to do more crazy things, if the transaction is trying to call the deployer system contract, we can do a system call instead of a regular call, this is just an example of how we can customize the execution logic based on the callee, we can also add more custom logic for different callees or even based on the data or value of the transaction
                 uint32 gas = Utils.safeCastToU32(gasleft());
                 SystemContractsCaller.systemCallWithPropagatedRevert(gas, to, value, data);
             } else {
                 bool success;
             assembly {
-                success := call(gas(), to, value, add(data, 0x20), mload(data), 0, 0)
+                success := call(gas(), to, value, add(data, 0x20), mload(data), 0, 0) // this is how we make calls it's just in a really low level lingo in zkSync
 
             }  
             if(!success) {
                 revert ZkMinimalAccount__ExecutionFailed();
             }
             }
-
-        }
-
-    function executeTransactionFromOutside(Transaction memory _transaction) external payable
-    {}
-
-    function payForTransaction(bytes32 _txHash, bytes32 _suggestedSignedHash, Transaction memory _transaction)
-        external
-        payable
-        {}
-
-    function prepareForPaymaster(bytes32 _txHash, bytes32 _possibleSignedHash, Transaction memory _transaction)
-        external
-        payable
-        {}
+    }
 }
